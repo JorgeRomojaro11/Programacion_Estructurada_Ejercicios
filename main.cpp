3c731bb59eb09cf83d206b2fed261829f48f5c88
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-12 11:39:52 +0100

    .

--- main.cpp
+++ main.cpp
@@ -12,5 +12,9 @@ estudiante1.edad = 20;
 estudiante1.promedio = 85.5;
 strcpy(estudiante1.nombre, "Marta Diaz");
 
+struct Estudiante *estudiante2 = (struct Estudiante *)malloc(sizeof(struct Estudiante));
+estudiante2->edad = 22;
+estudiante2->promedio = 90.0;
+strcpy(estudiante2->nombre, "Xokas Garcia");
 
 
