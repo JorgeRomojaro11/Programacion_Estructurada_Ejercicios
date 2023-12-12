d8ff7bc08674a1082bbf7ec845ff5ba64bcdcc51
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-12 12:04:45 +0100

    .

--- Estructuras_y_Funciones.cpp
+++ Estructuras_y_Funciones.cpp
@@ -6,3 +6,13 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+void imprimirEstudiantePorValor(struct Estudiante estudiante) {
+    printf("a. Por Valor: Nombre: %s, Edad: %d, Promedio: %f\n", estudiante.nombre, estudiante.edad, estudiante.promedio);
+}
+void imprimirEstudiantePorReferencia(struct Estudiante *estudiante) {
+    printf("b. Por Referencia: Nombre: %s, Edad: %d, Promedio: %f\n", estudiante->nombre, estudiante->edad, estudiante->promedio);
+}
+void imprimirEstudiantePorDireccion(struct Estudiante *estudiante) {
+    printf("c. Por Dirección: Nombre: %s, Edad: %d, Promedio: %f\n", estudiante->nombre, estudiante->edad, estudiante->promedio);
+}
