17016cb94e200f8c626be41fdeafa83918cf9929
JorgeRomojaro11 <jromodel@myuax.com>
2023-12-12 11:39:20 +0100

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
 };
+
+struct Estudiante estudiante1;
+estudiante1.edad = 20;
+estudiante1.promedio = 85.5;
+strcpy(estudiante1.nombre, "Marta Diaz");
+
+
+
